0199cdc50b85bc1fa80ed1ef7635946fd7744b0d
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-02-24 21:09:39 +0100

    feat: add sudoku controller

--- src/main/java/com/kfiatek/sudokuapi/controllers/SudokuController.java
+++ src/main/java/com/kfiatek/sudokuapi/controllers/SudokuController.java
@@ -1,4 +1,15 @@
 package com.kfiatek.sudokuapi.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sudoku")
 public class SudokuController {
+
+  @GetMapping("/hello")
+  public String sayHello() {
+    return "Witaj w świecie sudoku!";
+  }
 }
