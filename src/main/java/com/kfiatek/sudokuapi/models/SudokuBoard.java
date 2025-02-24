ab344c875795e63ffe1f6d501fde0a03db638687
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-02-24 21:09:45 +0100

    feat: add sudoku board

--- src/main/java/com/kfiatek/sudokuapi/models/SudokuBoard.java
+++ src/main/java/com/kfiatek/sudokuapi/models/SudokuBoard.java
@@ -1,4 +1,17 @@
 package com.kfiatek.sudokuapi.models;
 
 public class SudokuBoard {
+  private int[][] board;
+
+  public SudokuBoard() {
+    this.board = new int[9][9];
+  }
+
+  public int[][] getBoard() {
+    return board;
+  }
+
+  public void setBoard(int[][] board) {
+    this.board = board;
+  }
 }
